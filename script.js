bbe31c468167a86c8d87a519f64648a07a78a232
ayman Shibani Al Dujaili <ayman.shibani@student.kdg.be>
2024-04-18 23:41:42 +0200

    I added the structure for the index and some stuff more

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("btnDownload").addEventListener("click", function () {
+  // Actions when clicking the download button
+  alert("Download started!");
+  // Here you could add code to initiate the actual download of the game
+});
+
+document
+  .getElementById("btnDownloadFinal")
+  .addEventListener("click", function () {
+    // Actions when clicking the download button at the bottom of the page
+    alert("Download started!");
+    // Here you could add code to initiate the actual download of the game
+  });
