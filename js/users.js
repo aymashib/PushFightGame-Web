a72c30871e85cc0d17dc64e6f43a348a6210c91c
Markus Tiedemann <markus.tiedemann@student.kdg.be>
2024-05-06 21:49:08 +0200

    login

---
+++ js/users.js
@@ -0,0 +1,62 @@
+export const usersJSON = [
+  {
+    "id": 1,
+    "name": "Safa",
+    "password": "afas",
+    "isAdmin": false
+  },
+  {
+    "id": 2,
+    "name": "Ivan",
+    "password": "navi",
+    "isAdmin": true
+  },
+  {
+    "id": 3,
+    "name": "NEWPLAYER",
+    "password": "reyalpwen",
+    "isAdmin": false
+  },
+  {
+    "id": 4,
+    "name": "ONLINEDATABASE",
+    "password": "esabatadenilno",
+    "isAdmin": false
+  },
+  {
+    "id": 5,
+    "name": "Uuesti",
+    "password": "itseuu",
+    "isAdmin": false
+  },
+  {
+    "id": 6,
+    "name": "ema",
+    "password": "ame",
+    "isAdmin": false
+  },
+  {
+    "id": 7,
+    "name": "Mama",
+    "password": "amam",
+    "isAdmin": false
+  },
+  {
+    "id": 8,
+    "name": "Papa",
+    "password": "apap",
+    "isAdmin": false
+  },
+  {
+    "id": 9,
+    "name": "Mar",
+    "password": "ram",
+    "isAdmin": false
+  },
+  {
+    "id": 10,
+    "name": "Rah",
+    "password": "har",
+    "isAdmin": false
+  }
+]
